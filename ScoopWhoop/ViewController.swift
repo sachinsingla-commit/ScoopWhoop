7813c953833fb89139c34f80e4c5406e5d9830ba
sachin <ssingla636@gmail.com>
2021-06-22 21:07:07 +0530

    Initial Commit

---
+++ ScoopWhoop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoopWhoop
+//
+//  Created by Sachin's Macbook Pro on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
